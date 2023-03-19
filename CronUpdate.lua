5aff8ae81a8558868aab3d68b3a4abe37ec686cd
Vladgobelen <vladgobelen@gmail.com>
2023-03-20 06:37:03 +0700

    работает, узнать как сделать цикл с переменной

--- CronUpdate.lua
+++ CronUpdate.lua
@@ -7,46 +7,40 @@ local hour = io.popen("date +%H", 'r');
 local hour_data = tonumber(hour:read('*a'));
 --статичная прибавка времени
 local minut_plus = 30;
+local minut_plus_b = 5;
 local hour_plus = 2;
 local hour_plus_b = 3;
 --переменые для требуемого времени
 local hour_new;
-local minut_new
-
---отладка
-print (minut_data);
-print (hour_data);
---отладка
-
+local minut_new;
+local minut_new_b;
 --проверяем количество минут
 if minut_data >= 30 then
 --если количество текущих минут больше 30, то к часам прибавляем 3, а от текущего времени отнимаем 30 минут
     hour_new = hour_data + hour_plus_b;
     minut_new = minut_data - minut_plus;
+    minut_new_b = minut_new + minut_plus_b;
 else
 --если количество текущих минут меньше 30, то к часам прибавляем 2, а к текущим минутам 30
     hour_new = hour_data + hour_plus;
     minut_new = minut_data + minut_plus;
+    minut_new_b = minut_new + minut_plus_b;
 end
 
---отладка
-print (minut_new);
-print (hour_new);
--- отладка
-
----формируем строку для крона
+---формируем строки для крона
 local cron_time = minut_new .. " " .. hour_new .. " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh";
-
---отладка
-print (cron_time);
---отладка
+local cron_time1 = minut_new_b .. " " .. hour_new .. " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh";
+local cron_time2 = "07 11 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh";
+local cron_time3 = "07 23 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh";
+local cron_time4 = "14 03 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh";
+local cron_time5 = "14 15 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh";
 
 ---записать переменную в файл
-local cronFile = io.open("/home/diver/Скрипты/temp/cronf", "r")
-cronFile:write (cron_time, '\n')
-cronFile:close()
-
-
-
-
-
+local cronFile = io.open("/home/diver/Скрипты/temp/cronf", "w");
+cronFile:write (cron_time, '\n');
+cronFile:write (cron_time1, '\n');
+cronFile:write (cron_time2, '\n');
+cronFile:write (cron_time3, '\n');
+cronFile:write (cron_time4, '\n');
+cronFile:write (cron_time5, '\n');
+cronFile:close();
