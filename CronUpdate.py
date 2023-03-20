bf2c0e8ac195c4c3d0617473e0e2976645ab3399
Vladgobelen <vladgobelen@gmail.com>
2023-03-20 22:54:46 +0700

    работает

--- CronUpdate.py
+++ CronUpdate.py
@@ -1,15 +1,16 @@
 import datetime;
-minNew = (datetime.datetime.now() + datetime.timedelta(hours=2, minutes=30)).strftime("%M");
-hourNew = (datetime.datetime.now() + datetime.timedelta(hours=2, minutes=30)).strftime("%H");
-minNewB = (datetime.datetime.now() + datetime.timedelta(hours=0, minutes=5)).strftime("%M");
-hourNewB = (datetime.datetime.now() + datetime.timedelta(hours=0, minutes=0)).strftime("%H");
+minNew = (datetime.datetime.now() + datetime.timedelta(hours=2, minutes=30)).strftime("%M")
+hourNew = (datetime.datetime.now() + datetime.timedelta(hours=2, minutes=30)).strftime("%H")
+minNewB = (datetime.datetime.now() + datetime.timedelta(hours=0, minutes=5)).strftime("%M")
+hourNewB = datetime.datetime.now().strftime("%H")
+cronNew = open('/var/spool/cron/crontabs/diver', 'w')
 
-cronNew = open('/home/diver/Скрипты/temp/cron', 'w');
+cronNew.write(minNew + " " + hourNew + " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write(minNewB + " " + hourNewB + " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("10 11 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("10 23 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("14 03 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("14 15 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write(minNew + " " + hourNew + " * * * env DISPLAY=:0 python /home/diver/Скрипты/CronUpdate/CronUpdate.py" + "\n")
 
-cronNew.write(minNew + " " + hourNew + " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n");
-cronNew.write(minNewB + " " + hourNewB + " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n");
-cronNew.write("10 11 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n");
-cronNew.write("10 23 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n");
-cronNew.write("14 03 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n");
-cronNew.write("14 15 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n");
 cronNew.close();
