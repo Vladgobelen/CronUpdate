4b8630ff3424cebe43fcd0438822a5ced38b77d1
Vladgobelen <vladgobelen@gmail.com>
2023-03-21 03:35:14 +0700

    форма с выводом содержимого крон-файла

---
+++ ShowCron.py
@@ -0,0 +1,24 @@
+import sys
+import os
+from PyQt5 import QtWidgets
+import design
+
+cronText = open('/var/spool/cron/crontabs/diver', 'r')
+
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+
+        super().__init__()
+        self.setupUi(self)
+        self.textEdit.append (cronText.read())
+        cronText.close()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+     main()
