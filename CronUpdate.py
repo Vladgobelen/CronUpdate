ed17f0dc1ec23700bec4b1211802c3ccedbeb570
Vladgobelen <vladgobelen@gmail.com>
2023-04-20 20:08:24 +0700

    починка времени

--- CronUpdate.py
+++ CronUpdate.py
@@ -2,17 +2,17 @@ import datetime;
 minNew = (datetime.datetime.now() + datetime.timedelta(hours=2, minutes=30)).strftime("%M")
 hourNew = (datetime.datetime.now() + datetime.timedelta(hours=2, minutes=30)).strftime("%H")
 minNewB = (datetime.datetime.now() + datetime.timedelta(hours=0, minutes=5)).strftime("%M")
-hourNewB = datetime.datetime.now().strftime("%H")
+hourNewB= (datetime.datetime.now() + datetime.timedelta(hours=0, minutes=5)).strftime("%H")
 cronNew = open('/var/spool/cron/crontabs/diver', 'w')
 #отладочный файл
 #cronNew = open('/home/diver/Скрипты/temp/testCron', 'w')
 
 cronNew.write(minNew + " " + hourNew + " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
 cronNew.write(minNewB + " " + hourNewB + " * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
-cronNew.write("10 11 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
-cronNew.write("10 23 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
-cronNew.write("14 03 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
-cronNew.write("14 15 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("15 13 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("15 01 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("14 04 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
+cronNew.write("14 16 * * * env DISPLAY=:0 sh /home/diver/Скрипты/alarm.sh" + "\n")
 cronNew.write(minNew + " " + hourNew + " * * * env DISPLAY=:0 python /home/diver/Скрипты/CronUpdate/CronUpdate.py" + "\n")
 
 cronNew.close();
--- ShowCron.py
+++ ShowCron.py
@@ -14,11 +14,20 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.textEdit.append (cronText.read())
         cronText.close()
 
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = ExampleApp()
     window.show()
     app.exec_()
+
+def keyPressEvent(self, event):
+        """Close application from escape key.
+
+        results in QMessageBox dialog from closeEvent, good but how/why?
+        """
+        if event.key() == Qt.Key_Escape:
+            self.close()
 
 if __name__ == '__main__':
      main()
